0e1e1beae5b86f3bf778f4c357936aee2cafe457
Zac <a19860101@gmail.com>
2023-04-26 19:26:54 +0800

    20230426

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="{{route('category.store')}}" method="post">
+            @csrf
+            <div>
+                <label for="">title</label>
+                <input type="text" name="title">
+            </div>
+            <div>
+                <label for="">slug</label>
+                <input type="text" name="slug">
+            </div>
+            <input type="submit">
+        </form>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <th>title</th>
+                <th>slug</th>
+            </tr>
+            @foreach($categories as $category)
+            
+                <tr>
+                    <form action="{{route('category.update',$category->id)}}" method="post">
+                        @csrf
+                        @method('put')
+                        <td><input type="text" name="title" value="{{$category->title}}"></td>
+                        <td><input type="text" name="slug" value="{{$category->slug}}"></td>
+                        <td><input type="submit" value="update"></td>
+                    </form>
+                    <form action="{{route('category.destroy',$category->id)}}" method="post">
+                    <td>
+                            @csrf 
+                            @method('delete')
+                            <input type="submit" value="delete">
+                        </td>
+                    </form>
+                </tr>
+            
+            @endforeach
+        </table>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,5 @@ Route::get('/', function () {
 // Route::resource('/product',ProductController::class);
 
 Route::resource('/post',PostController::class);
+Route::resource('/category',CategoryController::class);
 
