04c9d0088a508ca82271a641ca975b738ad3d358
Zac <a19860101@gmail.com>
2023-05-10 19:25:10 +0800

    20230510

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,7 @@ class Post extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
